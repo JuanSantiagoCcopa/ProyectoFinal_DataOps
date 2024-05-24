f21ec7203eb55326612ce72059519b84fc6edec6
Juan Santiago Ccopa <juanju295@gmail.com>
2024-05-23 21:56:49 -0500

    Se carga archivo script para carga de dataset

---
+++ python/python_script.py
@@ -0,0 +1,33 @@
+import os
+
+## Carpeta dataset ##
+
+location= 'C:/Users/Galgo.DESKTOP-NU9OB99/Documents/proyecto_parcial/python/dataset'
+
+### Validar si existe carpeta ###
+if not os.path.exists(location): ##carpeta no existe
+    ##creo la carpeta
+    os.mkdir(location)
+else: ##carpeta existe
+    ##borrar contenido
+    for root, dirs, files in os.walk(location, topdown=False):
+        for name in files:
+            os.remove(os.path.join(root, name)) ## eliminar los archivos
+        for name in dirs:
+            os.rmdir(os.path.join(root, name)) ## eliminar las carpetas
+
+### Importar libreria API Kaggle ###
+
+from kaggle.api.kaggle_api_extended import KaggleApi
+
+### Autenticarnos ###
+
+api = KaggleApi()
+api.authenticate()
+
+### Descargar dataset ###
+
+
+
+
+
